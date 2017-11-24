05410717fdcfb03b35de6d342a55a79ea020d901
SaruKuroi <200196318@student.georgianc.on.ca>
2017-11-24 13:26:05 -0500

    first commit

---
+++ Practice/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoop1 = new System.Windows.Forms.Button();
+            this.lblLoop1 = new System.Windows.Forms.Label();
+            this.btnWhileLoop = new System.Windows.Forms.Button();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblSum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLoop1
+            // 
+            this.btnLoop1.Location = new System.Drawing.Point(12, 70);
+            this.btnLoop1.Name = "btnLoop1";
+            this.btnLoop1.Size = new System.Drawing.Size(75, 23);
+            this.btnLoop1.TabIndex = 0;
+            this.btnLoop1.Text = "For Loop";
+            this.btnLoop1.UseVisualStyleBackColor = true;
+            this.btnLoop1.Click += new System.EventHandler(this.btnLoop1_Click);
+            // 
+            // lblLoop1
+            // 
+            this.lblLoop1.AutoSize = true;
+            this.lblLoop1.Location = new System.Drawing.Point(12, 24);
+            this.lblLoop1.Name = "lblLoop1";
+            this.lblLoop1.Size = new System.Drawing.Size(144, 13);
+            this.lblLoop1.TabIndex = 1;
+            this.lblLoop1.Text = "Click the Button to run a loop";
+            // 
+            // btnWhileLoop
+            // 
+            this.btnWhileLoop.Location = new System.Drawing.Point(217, 69);
+            this.btnWhileLoop.Name = "btnWhileLoop";
+            this.btnWhileLoop.Size = new System.Drawing.Size(75, 23);
+            this.btnWhileLoop.TabIndex = 2;
+            this.btnWhileLoop.Text = "While Loop";
+            this.btnWhileLoop.UseVisualStyleBackColor = true;
+            this.btnWhileLoop.Click += new System.EventHandler(this.btnWhileLoop_Click);
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(12, 164);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(100, 20);
+            this.txtX.TabIndex = 3;
+            this.txtX.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(12, 190);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(100, 20);
+            this.txtY.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(172, 186);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lblSum
+            // 
+            this.lblSum.AutoSize = true;
+            this.lblSum.Location = new System.Drawing.Point(313, 186);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(161, 13);
+            this.lblSum.TabIndex = 6;
+            this.lblSum.Text = "Click the Button to Add Numbers";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 505);
+            this.Controls.Add(this.lblSum);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.btnWhileLoop);
+            this.Controls.Add(this.lblLoop1);
+            this.Controls.Add(this.btnLoop1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoop1;
+        private System.Windows.Forms.Label lblLoop1;
+        private System.Windows.Forms.Button btnWhileLoop;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblSum;
+    }
+}
+
---
+++ Practice/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLoop1_Click(object sender, EventArgs e)
+        {
+            //Use a loop to display numbers from 100 to 1020 on our label
+            //For loop structure: Start number, condition for continuing, Increment
+            lblLoop1.Text = "";
+            //counter++ is the same as counter = counter + 1
+            for (Int32 counter = 100; counter <= 120; counter++)
+            {
+                // add the current number on to the end of the label text
+                lblLoop1.Text += counter + " ";
+            }
+
+        }
+
+        private void btnWhileLoop_Click(object sender, EventArgs e)
+        {
+            lblLoop1.Text = "";
+            Int32 counter = 100;
+            while (counter <= 120)
+            {
+                lblLoop1.Text += counter + " ";
+                //Add one to the counter
+                counter++;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //Capture x and y as variabes
+            Int32 x = Convert.ToInt32(txtX.Text);
+            Int32 y = Convert.ToInt32(txtY.Text);
+            //Send x and y to theAddNumbers method and display the result
+            Int32 result = AddNumbers(x, y);
+            lblSum.Text = Convert.ToString(result);
+        }
+        //Method accepts 2 integer inputs and returns another integer back
+        private Int32 AddNumbers(Int32 x, Int32 y)
+        {
+            Int32 sum = x + y;
+            return sum;
+        }
+    }
+}
